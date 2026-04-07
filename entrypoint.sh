3ba768feb04e9980caa34c52c49850f7b8413506
T_Ambroisine <104830706+TAmbroisine@users.noreply.github.com>
2026-04-07 11:17:49 +0200

    add fi

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,5 @@ VISIBILITY=$(jq -r '.repository.visibility' "$GITHUB_EVENT_PATH")
 if [[ "$VISIBILITY" == "private" ]]; then
     echo "🔒 This is a PRIVATE repository."
 else
-    echo "🌍 This is a PUBLIC repository." 
+    echo "🌍 This is a PUBLIC repository."
+fi
